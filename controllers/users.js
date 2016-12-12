d1dc20cf3889aac5358bffabd73eab836a413268
Tin Tran <tin.tran0519@gmail.com>
2016-12-11 19:49:58 -0800

    updated users controller and model

--- controllers/users.js
+++ controllers/users.js
@@ -1,29 +1,98 @@
 // Require resource's model(s).
 var User = require("../models/user");
 
-var index = function(req, res, next){
+//===============
+//Get all users
+var index = function(req, res){
   User.find({}, function(err, users) {
-    if (err) {
-      res.json({message: err});
-    } else {
-      res.render('users/index', {users: users});
-    }
+    if (err) res.send(err);
+
+    // return the users
+    res.json(users);
   });
 };
-
-var show = function(req, res, next){
+//===============
+//Get a user
+var show = function(req, res) {
   User.findById(req.params.id, function(err, user) {
-    if (err) {
-      res.json({message: 'Could not find user because ' + err});
-    } else if (!user) {
-      res.json({message: 'No user with this id.'});
-    } else {
-      res.render('users/show', {user: user});
-    }
+    if (err) res.send(err);
+
+    // return that user
+    res.json(user);
+
   });
 };
+//===============
+//Create User
+var create = function(req, res) {
+  var user        = new User(); //create a new instance of user model
+  user.name       = req.body.name; //set the users name
+  user.email      = req.body.email; //set users email
+  user.password   = req.body.password; //set the users pw
 
+  user.save(function(err, newUser) {
+    if (err) {
+      //duplicate entry
+      if(err.code == 11000)
+        return res.json({ success: false, message: 'A user with that info already exists!' })
+      else
+        return res.json(err);
+    }
+
+      var token = jwt.sign({
+        email: newUser.email,
+        name:  newUser.name,
+        _id:   newUser._id
+      }, topSecret, {
+        expiresIn: '30d'
+      })
+
+      //return a message after token created
+      res.json({
+        message: "Welcome to Web Reels!",
+        user: newUser,
+        token: token
+      });
+    });
+};
+//================
+//Update user
+var update = function(req, res) {
+  User.findById(req.params.id, function(err, user) {
+
+      if (err) res.send(err);
+
+      //set the new user information if it exists in the req
+      if (req.body.name)      user.name     = req.body.name;
+      if (req.body.email)     user.email    = req.body.email;
+      if (req.body.password)  user.password = req.body.password;
+
+      //save the user
+      user.save(function(err) {
+        if (err) res.send(err);
+
+        //return a message
+        res.json({ message: 'User updated!'})
+      });
+  });
+}
+//===============
+//Delete User
+var userDelete = function(req, res) {
+  User.remove({
+        ._id: req.params.id
+      }, function(err, user) {
+        if (err) res.send(err);
+
+        res.json({ message: 'User successfully delete' });
+  });
+}
+
+//===============
 module.exports = {
-  index: index,
-  show:  show
+  index:      index,
+  show:       show,
+  create:     create,
+  update:     update,
+  userDelete: userDelete
 };
--- models/user.js
+++ models/user.js
@@ -1,10 +1,50 @@
 var mongoose = require('mongoose'),
     debug    = require('debug')('app:models');
+    bcrypt   = require('bcrypt-nodejs');
 
+//===========================
+//User Schema
 var userSchema = new mongoose.Schema({
-  name:   String,
-  handle: String
+  name:   { type: String, required: true },
+  email:  { type: String, required: true },
+  password: { type: String, required: true, select: false }
 });
+
+//exclude password
+userSchema.set('toJSON', {
+  transform: function(doc, ret) {
+    delete ret.password;
+    return ret;
+  }
+});
+
+//hash pw before user is saved in db
+userSchema.pre('save', function(next) {
+  var user = this;
+
+  //if statement for hashing pw if user is new or pw changed
+  if (!user.isModified('password')) return next();
+
+  //generate the hash
+  bcrypt.hash(user.password, null, null, function(err, hash) {
+    if (err) return next(err);
+
+    //change the pw to the hashed version
+    user.password = hash;
+    next();
+  });
+});
+
+//method to compare a given pw with the database hash
+userSchema.methods.comparePassword = function(password) {
+  var user = this;
+
+  return bcrypt.compareSync(password, user.password);
+};
+
+//put user's movie list here
+//{TO DO}
+
 
 var User = mongoose.model('User', userSchema);
 
