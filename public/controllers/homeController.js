37b000792cb4085e54b1b74207c7ed980d62b9b7
Tin Tran <tin.tran0519@gmail.com>
2016-12-13 10:53:44 -0800

    dead link catch statement added

--- public/controllers/homeController.js
+++ public/controllers/homeController.js
@@ -5,9 +5,9 @@
     .module('webReels')
     .controller('HomeController', HomeController);
 
-    HomeController.$inject = ["$state", "userDataService", "$log", "$http"]
+    HomeController.$inject = ["$state", "userDataService", "$log", "$http","$window"]
 
-    function HomeController($state, userDataService, $log, $http){
+    function HomeController($state, userDataService, $log, $http, $window){
       var vm = this;
 
       vm.featuredMovies = [];
@@ -17,13 +17,34 @@
       vm.getMovies();
 
       function getMovies() {
+        console.log('working');
         $http.get('/featured')
           .then(function(res) {
-            console.log(res);
+            console.log(res.data.status_code);
+            if (res.data.status_code === 34) {
+              console.log('if statement working');
+              $('.jumbotron.home').css('background-image', `url(https://image.tmdb.org/t/p/original/xu9zaAevzQ5nnrsXN6JcahLnG4i.jpg)`);
+              return;
+            }
+            else {
+            console.log(res.data.backdrop_path);
             vm.featuredMovies = res.data;
-          }, function(err) {
+            vm.movieImage = res.data.backdrop_path;
+            return vm.movieImage;
+            }
+            }, function(err) {
             console.error('Error retrieving movie');
-          });
-    }
+          })
+          .then(function(res) {
+            if (res === undefined) return;
+            console.log('movieImage link', res);
+            $('.jumbotron.home').css('background-image', `url(https://image.tmdb.org/t/p/original${res})`);
+          }, function(err) {
+            console.error('Error');
+          })
+      }
+
+
+
   }
 })();
--- public/controllers/loginController.js
+++ public/controllers/loginController.js
@@ -23,23 +23,23 @@
 
       function login() {
         $('#login').modal('hide');
-        $http.get('/featured')
-          .then(function(res) {
-            console.log(res.data.backdrop_path);
-            vm.featuredMovies = res.data;
-            vm.movieImage = res.data.backdrop_path;
-            return vm.movieImage;
-          }, function(err) {
-            console.error('Error retrieving movie');
-          })
-          .then(function(res) {
-            console.log('movieImage link', res);
-            $('.jumbotron.home').css('background-image', `url(https://image.tmdb.org/t/p/w1280${res})`);
+        // $http.get('/featured')
+        //   .then(function(res) {
+        //     console.log(res.data.backdrop_path);
+        //     vm.featuredMovies = res.data;
+        //     vm.movieImage = res.data.backdrop_path;
+        //     return vm.movieImage;
+        //   }, function(err) {
+        //     console.error('Error retrieving movie');
+        //   })
+          // .then(function(res) {
+            // console.log('movieImage link', res);
+            // $('.jumbotron.home').css('background-image', `url(https://image.tmdb.org/t/p/w1280${res})`);
             authService.login(vm.loginData.email, vm.loginData.password)
-            return res;
-          }, function(err) {
-            console.error('Error');
-          })
+            // return res;
+          // }, function(err) {
+          //   console.error('Error');
+          // })
           .then(function(res) {
             $log.log(res);
             // $state.go('homePage');
